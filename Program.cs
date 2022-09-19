eb9abbe98e06f13afb78518cc416f9f9d4f1e6d7
Tolmachev Denis <reinor1995@gmail.com>
2022-09-19 22:06:59 +0900

    Enter the Code

--- Program.cs
+++ Program.cs
@@ -8,3 +8,47 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+// Создание нового массива данных 
+string[] CreateArray(int lengthArray)
+{
+    string[] array = new string[lengthArray];
+    Console.WriteLine("Введите построчно " + lengthArray + " элемента массива");
+    for (int i = 0; i < lengthArray; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+// Подсчет элементов в массиве данных по заданным параметрам
+void CreateResultArray(string[] array)
+{
+    int count = 0;
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    // Выведение результата в терминал
+    string[] arrayResult = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayResult[index] = array[i];
+            Console.Write(arrayResult[index] + " ");
+            index++;
+
+        }
+    }
+
+}
+
+// Итог
+Console.WriteLine("Введите длину массива"); // Запрос величины массива
+int lengthArray = int.Parse(Console.ReadLine()); // Введение и принятие массива в обработку
+string[] array = CreateArray(lengthArray); // Обработка массива по заданным параметрам
+CreateResultArray(array); // Выведение результата в терминал
